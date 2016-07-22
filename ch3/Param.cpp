c49c64ce7394057b2be07e98f0340857f13acb09
Hai Tang <402696415@qq.com>
2016-07-21 19:42:15 -0500

    begin, end

--- ch3/Param.cpp
+++ ch3/Param.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <list>
+#include <complex>
+#include <vector>
+#include <iterator>
 using namespace std;
 // page 92
 // The template<typename T> prefix makes T a parameter of the declaration it prefixes
@@ -20,6 +23,10 @@ public:
 	T& operator[](int i);
 	const T& operator[](int i) const;
 	int size() const{return sz;}
+	// T* begin(Vector<T>& x);
+	// T* end(Vector<T>& x);
+	const T* begin() const {return &elem[0];}
+	const T* end() const{return &elem[0]+size();}
 };
 
 template<typename T>
@@ -36,6 +43,38 @@ const T& Vector<T>::operator[](int i) const
 	if(i<0 || size()<=i)
 		throw "Vector::operator[]";
 	return elem[i];
+}
+
+// template<typename T>
+// const T* Vector<T>::begin(Vector<T>& x) const
+// {
+// 	return &x[0];			// pointer to first element
+// }
+
+// template<typename T>
+// const T* Vector<T>::end(Vector<T>& x) const
+// {
+// 	return x.begin()+x.size();
+// }
+
+
+template<typename Container, typename Value>
+Value sum(const Container& c, Value v)
+{
+	for(auto x:c)
+		v+=x;
+	return v;
+}
+
+void user(Vector<int>& vi,list<double>& Id,vector<complex<double> >& vc)
+{
+
+}
+
+void f2(const Vector<string>& vs)
+{
+	for(auto& s:vs)
+		cout << s << '\n';
 }
 
 int main()
