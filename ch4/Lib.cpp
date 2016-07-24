cdb48115280fd434f550a6cb98a1374ab23cb7c4
Hai Tang <402696415@qq.com>
2016-07-24 18:54:13 -0500

    first commit in new house

---
+++ ch4/Lib.cpp
@@ -0,0 +1,20 @@
+#include<algorithm>		// copy(), find(), sort()
+#include<array>			// array
+#include<chrono>		// duration, time_point
+#include<cmath>			// sqrt(), pow()
+#include<complex>		// complex, sqrt(), pow()
+#include<fstream>		// fstream, ifstream, ofstream
+#include<future>		// future, promise
+#include<iostream>		// istream, ostream, cin, cout
+#include<map>			// map, multimap
+#include<memory>		// unique_ptr, shared_ptr, normal
+#include<random> 		// default_random_engine, normal_distribution
+#include<regex>			// regex, smatch
+#include<string>
+#include<set>			// set, multiset
+#include<sstream>		// istrstream, ostrstream
+#include<thread>
+#include<unordered_map>	// unordered_map, unordered_multimap
+#include<utility>		// move(), swap(), pair
+#include<vector>
+
---
+++ ch4/strings.cpp
@@ -0,0 +1,30 @@
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+string compose(const string& name, const string& domain)
+{
+	return name+'@'+domain;
+}
+
+void m2(string& s1, string& s2)
+{
+	s1 = s1+'\n';	// append newline
+	s2 += '\n';
+}
+
+string name = "Niels Stroustrup";
+
+void m3()
+{
+	string s = name.substr(6,10);
+	name.replace(0,5,"nicolas");
+	name[0] = toupper(name[0]);
+}
+
+int main()
+{
+	m3();
+	return 0;
+}
