56ead7e8cbecefc7b4958a744ef529c6405ac5ef
Hai Tang <402696415@qq.com>
2016-08-09 23:20:09 -0500

    iterator_trait example ii

--- ch5/Iterator_traits.cpp
+++ ch5/Iterator_traits.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 #include <iterator>
 #include <typeinfo>
-
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include <forward_list>
+// page 139
 using namespace std;
+
+// iterator_trait allow us to check what kind of 
+// iterator is supported
+
+//void test(vector<string>& v,forward_list<int>& lst)
+//{
+//	sort(v);
+//	sort(lst);
+//}
+
+template<class BidirIt>
+void my_reverse(BidirIt first, BidirIt last)
+{
+	typename iterator_traits<BidirIt>::difference_type n
+		=distance(first,last);
+	--n;
+	while(n > 0)
+	{
+		typename iterator_traits<BidirIt>::value_type tmp = *first;
+		*first++ = *--last;
+		*last = tmp;
+		n -= 2;
+	}
+}
 
 int main()
 {
@@ -10,5 +39,18 @@ int main()
 	if(typeid(traits::iterator_category) 
 		== typeid(random_access_iterator_tag))
 		cout << "int is a random-access iterator" << endl;
+	
+	vector<int> v{1,2,3,4,5};
+	my_reverse(v.begin(),v.end());
+	for(int n:v)
+		cout << n << ' ';
+	cout << endl;
+
+	list<int> l{1,2,3,4,5};
+	my_reverse(l.begin(),l.end());
+	for(auto n:l)
+		cout << n << ' ';
+	cout << '\n';
+
 	return 0;
 }
